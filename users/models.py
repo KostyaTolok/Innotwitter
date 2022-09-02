560c3da4271c450d27279473cae8e27ff0358852
Kostya <tolok.kostya@gmail.com>
2022-09-02 12:41:25 +0300

    add models

---
+++ users/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        USER = 'user'
+        MODERATOR = 'moderator'
+        ADMIN = 'admin'
+
+    email = models.EmailField(verbose_name="User email", unique=True)
+    image_path = models.URLField(verbose_name="User image", max_length=200, null=True, blank=True)
+    role = models.CharField(verbose_name="User role", max_length=9, choices=Roles.choices)
+    title = models.CharField(verbose_name="User title", max_length=80)
+    is_blocked = models.BooleanField(verbose_name="User is blocked", default=False)
+
+    def __str__(self):
+        return self.email
